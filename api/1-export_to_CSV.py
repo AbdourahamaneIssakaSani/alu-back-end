0b07430e1f7c2190922eb26ba7f29114b95aaec5
Abissa <a.sani@alustudent.com>
2022-10-18 06:24:51 -0700

    :robot: automated commit of 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,7 +5,7 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    employee_id = 2  # sys.argv[1]
+    employee_id = sys.argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users/{}" \
         .format(employee_id)
     todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
